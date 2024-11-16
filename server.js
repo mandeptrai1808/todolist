3c63a7f45fb8f9b4a32d4edc97d0335e42f5ebe4
John Smith <vanman12315@gmail.com>
2024-11-17 06:07:51 +0700

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// require('dotenv').config();
-// const app = require('./src/app');
-
-// //Start the server
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}.`);
-// });
-
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors'); // Import cors
@@ -15,9 +5,6 @@ const app = require('./src/app');
 
 // Cấu hình CORS - cho phép tất cả các nguồn gốc
 app.use(cors());
-
-// Hoặc giới hạn CORS cho các domain cụ thể (thay thế 'https://your-frontend-domain.com' bằng domain thực tế của bạn)
-// app.use(cors({ origin: 'https://your-frontend-domain.com' }));
 
 // Start the server
 const port = process.env.PORT || 3000;
--- src/database/knex.js
+++ src/database/knex.js
@@ -1,8 +1,8 @@
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 module.exports = require('knex')({
-  client: 'mysql', 
-  // client: 'pg', //deploy
+  // client: 'mysql', 
+  client: 'pg', //deploy
 
   connection: {
     host: DB_HOST,
@@ -10,6 +10,19 @@ module.exports = require('knex')({
     user: DB_USER,
     password: DB_PASS,
     database: DB_NAME,
+    
   },
   pool: { min: 0, max: 10 },
 });
+
+async function testDatabaseConnection() {
+  try {
+    await db.raw('SELECT 1'); // Thực hiện truy vấn kiểm tra
+    console.log('Database connected successfully!');
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+  }
+}
+
+// Gọi hàm này tại entry point (ví dụ: server.js)
+testDatabaseConnection();
